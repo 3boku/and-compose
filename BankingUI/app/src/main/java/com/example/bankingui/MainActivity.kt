1f637061e86c67c8829c3f36cd9108d85e49387f
정삼복 <68502584+3boku@users.noreply.github.com>
2024-05-16 23:55:52 +0900

    Finance

--- BankingUI/app/src/main/java/com/example/bankingui/MainActivity.kt
+++ BankingUI/app/src/main/java/com/example/bankingui/MainActivity.kt
@@ -66,7 +66,7 @@ fun HomeScreen() {
             WalletSection()
             Spacer(modifier = Modifier.height(16.dp))
             CardsSection()
-            //FinanceSection()
+            FinanceSection()
             //CurrenciesSection()
         }
 
